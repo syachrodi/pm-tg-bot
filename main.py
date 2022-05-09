c8489c3c1ba5131f4b410341efb51c32cd631ecd
aerofanz <syachrodi@gmail.com>
2022-05-09 09:22:43 +0700

    a

--- main.py
+++ main.py
@@ -1,32 +1,70 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-import os
-import requests
+from selenium import webdriver
+from pprint import pprint
 from bs4 import BeautifulSoup
+import time
 
-BASE_URL = 'https://www.kogan.com/nz/'
+BASE_URL = 'https://thesphynx.co/swap/'
 TOKEN = os.getenv('TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 TELEGRAM_API_SEND_MSG = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 
 items = [
-    'buy/kogan-35-curved-219-ultrawide-75hz-freesync-gaming-monitor-b/',
-    'buy/kogan-full-rgb-mechanical-keyboard-brown-switch/'
+    '32520/0xB361D5953e21Cfde5CD62B89FDf40bc21903A6bb/',
+    # '0x71946a5C9dA7C95ee804a9BE561EC15A3F286A7D/'
 ]
 
 def main(event={}, context={}):
     for item in items:
-        url = BASE_URL + item
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, 'html.parser')
+        url = "https://thesphynx.co/swap/32520/0xB361D5953e21Cfde5CD62B89FDf40bc21903A6bb/"
+        # headers = { 'User-Agent': 'Generic user agent' }
+        # page = requests.get(url, headers=headers)
+        driver = webdriver.Chrome()
+        driver.get(url)
 
-        name = soup.select_one('h1[itemprop="name"]').get_text()
-        price = soup.select_one('h3[itemprop="price"]')['content']
+        time.sleep(3)
+
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        pprint(soup)
+        # soup.find_all('div')[0].get_text()
+        # tes = 
+        # soup = BeautifulSoup(r.text, 'html.parser')
+        harga = ""
+        market = ""
+
+
+        # soup = BeautifulSoup(urllib.urlopen(r).read(), 'html.parser')
+        # res = soup.find_all('div', attrs={'class': 'sc-fSDTwv bijjgX'})
+        # mcaps = soup.find_all('div', attrs={'class': 'sc-gsDKAQ zTPCH'})
+
+
+        # for i in res:
+        #     price = i.find('div', attrs={"class":"sc-gsDKAQ QYecP"})
+        #     print('\ntitle:', price.text.strip())
+        #     harga = price.text.strip()
+
+        # for i in res:
+        #     mcap = i.find('div', attrs={"class":"sc-gsDKAQ zTPCH"})
+        #     print('\ntitle:', mcap.text.strip())
+        #     market = mcap.text.strip()
+
+        # str('soup.prettify()')
+        # print('\nPrice:', price.text.strip())
+        # print('\nMcap:', mcap.text.strip())
+        # price = soup.select_one('div.sc-gsDKAQ QYecP').text
+        # mcap = soup.select_one('div.sc-gsDKAQ zTPCH').text
+        # price = soup.select_one('h1[class="sc-gsDKAQ QYecP"]').text
+        # mcap = soup.select_one('h3[class="sc-gsDKAQ zTPCH"]').text
+        # price = soup.select_one('h3[class="sc-gsDKAQ zTPCH"]')['content']
 
         data = {
             'chat_id': CHAT_ID,
-            'text': f'*${price}*\n[{name}]({url})',
+            # 'text': soup.prettify(),
+
+            'text': f'*${soup.prettify()}*\n({url})',
+            # 'text': f'*${harga}*\n[{market}]({url})',
             'parse_mode': 'Markdown'
         }
         r = requests.post(TELEGRAM_API_SEND_MSG, data=data)
